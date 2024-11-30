b5442ccbfc7f03c93c756c012d68c2907d4f51e6
yeti.thecoder <yeti.thecoder@gmail.com>
2024-11-30 13:55:56 +0100

    feature : filter

--- p2/README.md
+++ p2/README.md
@@ -1,6 +1,8 @@
 # P2 - Modal
 
-## Javascript y DOM
+`Javascript y DOM`
+
+## Descripción
 
 En este proyecto aplicaremos los conocimientos de JavaScript y DOM para crear una página que responda a cambios aplicados por un usuario y sea totalmente funcional.  
 Para ello, **haremos un "modal" que contenga el formulario de los filtros**, para que podamos verlo u ocultarlo a nuestro gusto mediante un click.  
@@ -9,7 +11,19 @@ También, tendremos una serie de productos pintados (utilizando DOM y teniendo u
 *Es importante que el código sea limpio, claro y mantenga buenas prácticas.*
 
 Os aportamos [este Figma](https://www.figma.com/design/e9oKogSc0W6koZmUqIbwv3/PROYECTO2?node-id=0-1&t=O64qsrsxQ40Qh06Y-1) para que podáis replicar el diseño.  
-*Las personas que no quieran replicar el figma y hacer algo más libre, siempre podéis elegir otra temática, pero teniendo en cuenta que tiene que cumplir con los requisitos mínimos y con la funcionalidad de la página sugerida.*
+*Las personas que no quieran replicar el Figma y hacer algo más libre, siempre podéis elegir otra temática, pero teniendo en cuenta que tiene que cumplir con los requisitos mínimos y con la funcionalidad de la página sugerida.*
 [DISEÑO SUGERIDO](https://www.figma.com/design/e9oKogSc0W6koZmUqIbwv3/PROYECTO2?node-id=0-1&t=O64qsrsxQ40Qh06Y-1)  
 
 Para la entrega bastará con que subas tu proyecto a github y nos facilites en enlace en el input que tienes a la derecha. EL REPOSITORIO DE GITHUB DEBE ESTAR PÚBLICO PARA PODER REALIZAR LA CORRECCIÓN.
+
+## Requisitos
+
+[X] Nuestro proyecto consta de un archivo style.css, un index.html y un index.js  
+[X] Habrá un array de objetos con la información de los productos que queramos pintar  
+[] Web FULL RESPONSIVE  
+[] Cumple los requisitos mínimos del anterior proyecto en cuanto al HTML y el CSS  
+[] Se generan los productos recorriendo el array con un bucle y pintándolos en el HTML mediante DOM  
+[] Hay un mínimo de dos filtros que funcionen en conjunto  
+[] Si no se encuentra ningún producto con los filtros introducidos se mostrarán 3 productos aleatorios con un mensaje de productos sugeridos  
+[] Existe un botón limpiar filtros, que reinicia los inputs de filtrado y pinta todos los productos  
+[] El contenido del HTML es muy escueto, y como mucho tiene un header y un footer, el resto irá inyectado mediante javascript  
---
+++ p2/index.js
@@ -0,0 +1,262 @@
+// script.js
+
+const products = [
+  {
+    id: 1,
+    model: "Forum Buckle",
+    brand: "Adidas",
+    price: 100,
+    image: "./assets/adidas-forum_buckle-blue.png",
+  },
+  {
+    id: 2,
+    model: "Campus",
+    brand: "Adidas",
+    price: 120,
+    image: "./assets/adidas_campus-green.png",
+  },
+  {
+    id: 3,
+    model: "Gazelle",
+    brand: "Adidas",
+    price: 100,
+    image: "./assets/adidas-gazelle-pink.png",
+  },
+  {
+    id: 4,
+    model: "Handball Spezial",
+    brand: "Adidas",
+    price: 100,
+    image: "./assets/adidas-handball_spezial-black_pink.png",
+  },
+  {
+    id: 5,
+    model: "Handball Spezial",
+    brand: "Adidas",
+    price: 100,
+    image: "./assets/adidas-handball_spezial-grey_red.png",
+  },
+  {
+    id: 6,
+    model: "NB 9060",
+    brand: "New Balance",
+    price: 80,
+    image: "./assets/newBalance-NB_9060-grey.png",
+  },
+  {
+    id: 7,
+    model: "NB 327",
+    brand: "New Balance",
+    price: 120,
+    image: "./assets/newBalance-NB_327-white_green.png",
+  },
+  {
+    id: 8,
+    model: "NB 9060",
+    brand: "New Balance",
+    price: 100,
+    image: "./assets/newBalance-NB_9060-brown_pink.png",
+  },
+  {
+    id: 9,
+    model: "NB 480",
+    brand: "New Balance",
+    price: 150,
+    image: "./assets/newBalance-NB_480-white_red.png",
+  },
+  {
+    id: 10,
+    model: "Air Max 1",
+    brand: "Nike",
+    price: 120,
+    image: "./assets/nike-air_max_1-white_green.png",
+  },
+  {
+    id: 11,
+    model: "Nike Revolution",
+    brand: "Nike",
+    price: 90,
+    image: "./assets/nike-revolution-blue.png",
+  },
+  {
+    id: 12,
+    model: "Dunk Low",
+    brand: "Nike",
+    price: 100,
+    image: "./assets/nike-dunk_low-green_white.png",
+  },
+  {
+    id: 13,
+    model: "Air Max SC",
+    brand: "Nike",
+    price: 150,
+    image: "./assets/nike-air_max_sc-green.png",
+  },
+  {
+    id: 14,
+    model: "Air Force",
+    brand: "Nike",
+    price: 130,
+    image: "./assets/nike-air_force-red_white.png",
+  },
+  {
+    id: 15,
+    model: "Full Force Low",
+    brand: "Nike",
+    price: 70,
+    image: "./assets/nike-full_force_low-white_blue.png",
+  },
+  {
+    id: 16,
+    model: "Air Max SC",
+    brand: "Nike",
+    price: 160,
+    image: "./assets/nike-air_max_sc-grey.png",
+  },
+];
+
+function renderMainContent() {
+  const mainContent = document.getElementById("main-content");
+  const filterMenu = document.createElement("section");
+
+  filterMenu.className = "filter-menu";
+  filterMenu.innerHTML = `
+        <div class="filters" style="display: none;">
+            <div>
+                <label for="brand"></label>
+                <select id="brand">
+                    <option value="all">Todo</option>
+                    <option value="Adidas">Adidas</option>
+                    <option value="New Balance">New Balance</option>
+                    <option value="Nike">Nike</option>
+                </select>
+            </div>
+            <div>
+                <label for="price"></label>
+                <input type="input" id="price" min="0" max="250" value="250" oninput="updatePriceLabel(this.value)">
+                <span id="price-label">€</span> 
+            </div>
+            <button class="clear-filters" onclick="clearFilters()">Todo</button>
+        </div>
+
+        <div class="space-divider"></div>
+        
+        <div class="filter-icon" onclick="toggleFilterMenu()">
+          <input type="checkbox" id="down-up">
+          <label for="down-up" class="flipping-svg">
+              <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 75 75" fill="none">
+                <path d="M38.5008 51.3335C37.7512 51.3349 37.0247 51.0738 36.4475 50.5955L17.1975 34.5539C16.5423 34.0093 16.1302 33.2267 16.052 32.3784C15.9738 31.53 16.2358 30.6853 16.7804 30.0301C17.325 29.3749 18.1075 28.9629 18.9559 28.8847C19.8042 28.8065 20.6489 29.0684 21.3041 29.613L38.5008 43.9864L55.6975 30.1264C56.0256 29.8599 56.4033 29.6608 56.8086 29.5407C57.2139 29.4207 57.639 29.3819 58.0594 29.4266C58.4798 29.4713 58.8872 29.5987 59.2582 29.8013C59.6292 30.004 59.9565 30.278 60.2212 30.6076C60.515 30.9375 60.7376 31.3245 60.8749 31.7444C61.0121 32.1643 61.0612 32.608 61.0191 33.0477C60.9769 33.4875 60.8444 33.9138 60.6298 34.2999C60.4152 34.6861 60.1232 35.0237 59.772 35.2918L40.5221 50.788C39.9282 51.1907 39.2166 51.3828 38.5008 51.3335Z" fill="var(--yeti-p2-light-filter-arrow-color)"/>
+              </svg>
+          </label>
+        </div>
+    `;
+  mainContent.appendChild(filterMenu);
+
+  const productContainer = document.createElement("section");
+  productContainer.className = "product-cards";
+  productContainer.id = "product-container";
+  mainContent.appendChild(productContainer);
+
+  renderProducts(products);
+}
+
+
+
+function renderProducts(filteredProducts) {
+  const productContainer = document.getElementById("product-container");
+  productContainer.innerHTML = "";
+
+  if (filteredProducts.length === 0) {
+    const message = document.createElement("p");
+    message.innerText =
+      "No queda nada con las opciones que buscas, pero ¡Mira estas sugerencias para tí!";
+    productContainer.appendChild(message);
+
+    const suggestions = getRandomAlternatives();
+    renderSuggestionProducts(suggestions, productContainer);
+    return;
+  }
+
+  filteredProducts.forEach((product) => {
+    const card = createProductCard(product);
+    productContainer.appendChild(card);
+  });
+}
+
+function renderSuggestionProducts(suggestions, container) {
+  suggestions.forEach((product) => {
+    const card = createProductCard(product);
+    container.appendChild(card);
+  });
+}
+
+function createProductCard(product) {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+        <img src="${product.image}" alt="${product.model}">
+        <h3>${product.brand}</h3>
+        <p>Model: ${product.model}</p>
+        <p>Price: ${product.price} €</p>
+        <button onclick="buyProduct(${product.id})">Comprar</button>
+    `;
+  return card;
+}
+
+function filterProducts() {
+  const brandFilter = document.getElementById("brand").value;
+  const priceFilter = document.getElementById("price").value;
+
+  let filteredProducts = products;
+
+  if (brandFilter !== "all") {
+    filteredProducts = filteredProducts.filter(
+      (product) => product.brand === brandFilter
+    );
+  }
+
+  filteredProducts = filteredProducts.filter(
+    (product) => product.price <= priceFilter
+  );
+
+  renderProducts(filteredProducts);
+}
+
+function updatePriceLabel(value) {
+  const priceLabel = document.getElementById("price-label");
+  priceLabel.innerText = `${value} €`;
+}
+
+function clearFilters() {
+  document.getElementById("brand").value = "all";
+  document.getElementById("price").value = 250;
+  updatePriceLabel(250);
+  renderProducts(products);
+}
+
+function toggleFilterMenu() {
+  const filterMenu = document.querySelector(".filters");
+  filterMenu.style.display =
+    filterMenu.style.display === "none" ? "block" : "none";
+}
+
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu ul");
+  menu.style.display = menu.style.display === "none" ? "flex" : "none";
+}
+
+function getRandomAlternatives() {
+  // const randomProducts = [];
+  const shuffled = [...products].sort(() => 0.5 - Math.random()).slice(0, 3);
+  return shuffled;
+}
+
+function buyProduct(id) {
+  alert(`Product: ${id} has been added to the cart!`);
+}
+
+renderMainContent();
+
+document.getElementById("brand").addEventListener("change", filterProducts);
+document.getElementById("price").addEventListener("input", filterProducts);
